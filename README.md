5718c0af25cc4abda75a44209cd6da095a849998
Sonia Lin <sonia_lin@hotmail.com>
2017-08-07 15:27:39 -0500

    Enable searching for all categories in a case sensitive manner, and enable bolding matched keyword with different cases

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a simple search engine built on Sinatra. It takes two search parameters:
+- category: tickets, organizations, and users in the data directory
+- keyword: the keyword matches every field in the selected category
+
+To run the app: 
+    ruby z-search.rb
--- z-search.rb
+++ z-search.rb
@@ -4,9 +4,29 @@ require 'json'
 
 helpers do
   def bold_matched_keyword(txt, keyword)
-    new_txt = txt.to_s.gsub(keyword, "<strong>#{keyword}</strong>")
-    new_txt
+    if is_a_matching_field(txt, keyword)
+      txt_in_str = txt.to_s
+      matched_keyword_begin_index = txt_in_str.downcase.index(keyword.downcase)
+      matched_keyword_end_index = matched_keyword_begin_index + keyword.length
+      new_txt = txt_in_str.insert(matched_keyword_end_index, '</strong>')
+      new_txt = new_txt.insert(matched_keyword_begin_index, '<strong>')
+      new_txt
+    else
+      txt
+    end
   end
+
+  def select_previously_selected_category(category)
+    if params[:category] && params[:category] == category
+      "<option value='#{category}' selected='selected'>#{category.capitalize}</option>"
+    else
+      "<option value='#{category}'>#{category.capitalize}</option>"
+    end
+  end
+end
+
+def is_a_matching_field(field, keyword)
+  field.to_s.downcase.include? keyword.downcase
 end
 
 def data_path
@@ -19,19 +39,31 @@ def parse_json(directory_path, filename)
   JSON.parse(raw_content)
 end
 
-def tickets
-  parse_json(data_path, "tickets.json")
+def get_collections(category)
+  parse_json(data_path, "#{category}.json")
 end
 
-def users
-  parse_json(data_path, "users.json")
+def process_record(record)
+  record.values.flatten.map{|field| downcase_string_field(field)}
 end
 
-def organizations
-  parse_json(data_path, "organizations.json")
+def downcase_string_field(field)
+  if field.is_a? String
+    field.downcase
+  else
+    field
+  end
 end
 
 get "/" do
-  @tickets = tickets.select {|ticket| ticket.values.flatten.include? params[:search]}
-  erb :index
+  if params[:category]
+    keyword = params[:search]
+    @results = get_collections(params[:category]).select {|record| process_record(record).include? keyword.downcase }
+    if @results.empty?
+      session[:message] = "There is no record matching the search criteria."
+    end
+    erb :index
+  else
+    erb :home
+  end 
 end
